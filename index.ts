6700c3e70ba9fd35313226d4221e704bbfc52bb9
Ernesto Fava <e.fava13@gmail.com>
2025-02-02 14:52:50 -0400

    Connect to browser instance
    Esta función usa el navegador ya instalado en el equipo y el usuario por defecto.

--- index.ts
+++ index.ts
@@ -1,1 +1,43 @@
-console.log("Hello World");
+import puppeteer from "puppeteer-core";
+import "dotenv";
+
+const browserPath: string =
+  process.env.BROWSER_PATH ||
+  "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+const userDataDirPath: string =
+  process.env.USER_DATA ||
+  "C:\\Users\\Efava\\AppData\\Local\\Google\\Chrome\\User Data\\Default";
+
+(async () => {
+  let browser;
+
+  try {
+    browser = await puppeteer.launch({
+      headless: false,
+      executablePath: browserPath,
+      userDataDir: userDataDirPath,
+      slowMo: 100,
+    });
+
+    let browserWSEndpoint = browser.wsEndpoint();
+    console.log(browserWSEndpoint);
+
+    browser = await puppeteer.connect({
+      browserWSEndpoint: browserWSEndpoint,
+    });
+
+    const page = await browser.newPage();
+    await page.goto("https://linkedin.com/feed/");
+
+    //console.log(await browser.cookies());
+
+    browser.close();
+  } catch (error) {
+    console.error("Ocurrió un error:", error);
+  } finally {
+    if (browser) {
+      await browser.disconnect();
+      console.log("Navegador desconectado correctamente.");
+    }
+  }
+})();
