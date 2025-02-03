1c140e600dc5a73cda41cc51e8baf3ccc74156f3
Ernesto Fava <e.fava13@gmail.com>
2025-02-03 10:56:51 -0400

    logIn
    Inicio de sesión en formulario con variables de entorno.

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import puppeteer from "puppeteer-core";
-import "dotenv";
+import "dotenv/config";
 
 const browserPath: string =
   process.env.BROWSER_PATH ||
@@ -7,7 +7,9 @@ const browserPath: string =
 const userDataDirPath: string =
   process.env.USER_DATA ||
   "C:\\Users\\Efava\\AppData\\Local\\Google\\Chrome\\User Data\\Default";
-const user = process.env.USER || "";
+const name = process.env.NAME || "";
+const userName = process.env.USER || "";
+const password = process.env.PASSWORD || "";
 
 (async () => {
   let browser;
@@ -37,6 +39,20 @@ const user = process.env.USER || "";
         .filter((h1) => h1.innerText === "Iniciar sesión")
         .wait();
       console.log("Se detectó un formulario de inicio de sesión.");
+      await page.locator("#username").fill(userName);
+      console.log("Usuario ingresado");
+      await page.locator("#password").fill(password);
+      console.log("Contrasena ingresada");
+      await page.locator("#rememberMeOptIn-checkbox").fill("true");
+      console.log("Checkbox marcado");
+      await page.locator("button[type='submit']").click();
+      console.log("Iniciando sesión...");
+      await page.waitForSelector("h3", { timeout: 20000 });
+      await page
+        .locator("h3")
+        .filter((h3) => h3.innerText === name)
+        .wait();
+      console.log("Se detectó el feed.");
       // Aquí puedes agregar el código para manejar el formulario de inicio de sesión
     } catch (error) {
       console.log("No se detectó un formulario de inicio de sesión.");
@@ -48,7 +64,7 @@ const user = process.env.USER || "";
       await page.waitForSelector("h3", { timeout: 5000 });
       await page
         .locator("h3")
-        .filter((h3) => h3.innerText === user)
+        .filter((h3) => h3.innerText === name)
         .wait();
       console.log("Se detectó el feed.");
       // Aquí puedes agregar el código para manejar el formulario de inicio de sesión
